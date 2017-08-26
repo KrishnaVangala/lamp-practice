489846123f7536eacc521d2dcc16e0871ecab1dc
krishnavangala <saichaitanya6@gmail.com>
2017-08-26 12:19:08 -0400

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name 'build_cookbook'
+maintainer 'Krishna Vangala'
+maintainer_email 'saichaitanya6@gmail.com'
+license 'gplv3'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'delivery-truck'
